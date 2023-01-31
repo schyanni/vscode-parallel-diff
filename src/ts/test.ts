11cdbfdda4d67b044b66e16ee4e8ca44a78d7226
Yannick Schaffner <schyanni@ethz.ch>
2023-01-31 22:49:43 +0100

    update test.ts to new interface

--- src/ts/test.ts
+++ src/ts/test.ts
@@ -4,10 +4,10 @@ import { ChangeObject } from './common/change_object'
 import { Change } from 'diff'
 
 let left: string = 'abcd ich bin daheim'
-let right: string = 'bbcc ich bi diheim'
+let right: string = 'abcc ich bi diheim'
 
 let changeObjects: ChangeObject[] = greedy_diff(left, right);
-let changes : Change[] = default_diff(left, right);
+let changes : ChangeObject[] = default_diff(left, right);
 
 console.log("My implementation: ")
 console.log(JSON.stringify(changeObjects))
